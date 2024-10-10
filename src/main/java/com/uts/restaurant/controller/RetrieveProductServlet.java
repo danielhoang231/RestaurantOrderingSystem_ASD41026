c1ada06e2e2d7209b0a8ef6e580a94eee091ad11
danielhoang231 <126043298+danielhoang231@users.noreply.github.com>
2024-10-10 15:23:36 +1100

    Merging w/Main from Daniel Branch - code still to be combined

--- src/main/java/com/uts/restaurant/controller/RetrieveProductServlet.java
+++ src/main/java/com/uts/restaurant/controller/RetrieveProductServlet.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger; 
-import java.util.ArrayList;
 
 public class RetrieveProductServlet extends HttpServlet {
     @Override
